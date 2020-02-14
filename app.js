01bca53f21265dc0efd55016a81686314d0505be
Jasper Catthoor <kattoor.jc@gmail.com>
2020-02-14 13:46:28 +0100

    Rotating triangle

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const vertexShaderText = `
 precision mediump float;
 
-attribute vec2 vertPosition;
+attribute vec3 vertPosition;
 attribute vec3 vertColor;
 
 varying vec3 fragColor;
 
+uniform mat4 mWorld;
+uniform mat4 mView;
+uniform mat4 mProj;
+
 void main()
 {
     fragColor = vertColor;
-    gl_Position = vec4(vertPosition, 0.0, 1.0);
+    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
 }
 `;
 
@@ -27,8 +31,9 @@ void main()
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl');
 
-gl.clearColor(52 / 255, 152 / 255, 219 / 255, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+const vec3 = glMatrix.vec3;
+const mat4 = glMatrix.mat4;
+const toRadian = glMatrix.glMatrix.toRadian;
 
 const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderText);
 const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderText);
@@ -37,17 +42,46 @@ validateProgram(gl, program);
 
 createBuffer(gl,
     [
-        // x, y, r, g, b
-        0.0, 0.5, 1.0, 0.0, 0.0,
-        -0.5, -0.5, 0.0, 1.0, 0.0,
-        0.5, -0.5, 0.0, 0.0, 1.0]);
-
-setAttribute(gl, program, 'vertPosition', 2, 0);
-setAttribute(gl, program, 'vertColor', 3, 2);
+        // x, y, z, r, g, b
+        0.0, 0.5, 0.0, 1.0, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+        0.5, -0.5, 0.0, 0.0, 0.0, 1.0]);
 
 gl.useProgram(program);
-gl.drawArrays(gl.TRIANGLES, 0, 3);
 
+setAttribute(gl, program, 'vertPosition', 3, 0);
+setAttribute(gl, program, 'vertColor', 3, 3);
+
+const worldMatrix = mat4.create();
+
+const viewMatrix = mat4.create();
+mat4.lookAt(viewMatrix,
+    vec3.fromValues(0, 0, -2),
+    vec3.fromValues(0, 0, 0),
+    vec3.fromValues(0, 1, 0));
+
+const projectionMatrix = mat4.create();
+mat4.perspective(projectionMatrix, toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+
+setUniform(gl, program, 'mWorld', worldMatrix);
+setUniform(gl, program, 'mView', viewMatrix);
+setUniform(gl, program, 'mProj', projectionMatrix);
+
+const identityMatrix = mat4.create();
+let angle = 0;
+const loop = () => {
+    angle = performance.now() / 1000 / 6 * 2 * Math.PI; // full rotation (2*Math.PI) every 6 seconds
+    mat4.rotate(worldMatrix, identityMatrix, angle, vec3.fromValues(0, 1, 0));
+    setUniform(gl, program, 'mWorld', worldMatrix);
+
+    gl.clearColor(52 / 255, 152 / 255, 219 / 255, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    requestAnimationFrame(loop);
+};
+requestAnimationFrame(loop);
 
 /**
  *
@@ -121,9 +155,21 @@ function setAttribute(gl, program, attributeName, elementsPerAttribute, offset) 
     gl.vertexAttribPointer(
         attributeLocation,
         elementsPerAttribute,
-        gl.FLOAT, // type of elements
+        gl.FLOAT,
         false,
-        5 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
+        6 * Float32Array.BYTES_PER_ELEMENT,
         offset * Float32Array.BYTES_PER_ELEMENT);
     gl.enableVertexAttribArray(attributeLocation);
+}
+
+/**
+ *
+ * @param {WebGLRenderingContext} gl
+ * @param {WebGLProgram} program
+ * @param {string} uniformName
+ * @param {Float32List} matrix
+ */
+function setUniform(gl, program, uniformName, matrix) {
+    const uniformLocation = gl.getUniformLocation(program, uniformName);
+    gl.uniformMatrix4fv(uniformLocation, false, matrix);
 }
